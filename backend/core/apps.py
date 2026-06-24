88d881da7bd56ed2f3986e4ea7348aa6ea6cf69f
Haridh432 <harishsekar21460@gmail.com>
2026-06-24 18:16:18 +0530

    Auto create superuser on migrate

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_default_superuser(sender, **kwargs):
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        print("Auto-created admin user.")
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        post_migrate.connect(create_default_superuser, sender=self)
